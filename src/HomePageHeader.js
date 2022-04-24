f2cb948cccf6988fb911d94fd9329bd666431654
johannpa <johann_paque@hotmail.fr>
2022-04-24 14:43:12 +0200

    some issues

---
+++ src/HomePageHeader.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./App.css";
+
+const HomePageHeader = () => {
+    return (
+        <header className="header">
+            <h2>Your Stock Tracker</h2>
+        </header>
+    );
+};
+
+export default HomePageHeader;
